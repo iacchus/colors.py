f3d01093353e9afb7f2d6b8436d1326189d71e14
Iacchus Mercurius <kassivs.@gmail.com>
2020-07-15 10:37:54 -0300

    ok

--- colors/color.py
+++ colors/color.py
@@ -61,6 +61,8 @@ class Color(object):
 
     def from_hsv(self, hsv):
 
+        h, s, v = hsv
+
         if s > 1:
             raise ValueError('Saturation has to be less than 1')
         if v > 1:
@@ -103,52 +105,28 @@ class Color(object):
 
 
     def multiply(self, other):
-        #self_rgb = self.rgb
-        #other_rgb = other.rgb
-        #return RGBColor(
         return Color(
             rgb=tuple(map(lambda x, y: ((x * y) / 255.0),
                           zip(self.rgb, other.rgb)))
         )
-#         return Color(
-#             self_rgb.red * other_rgb.red / 255.0,
-#             self_rgb.green * other_rgb.green / 255.0,
-#             self_rgb.blue * other_rgb.blue / 255.0
-#         )
 
     __mul__ = multiply
 
     def add(self, other):
-#         self_rgb = self.rgb
-#         other_rgb = other.rgb
+
         return Color(
             rgb=tuple(map(lambda x, y: min(255, (x + y)),
                           zip(self.rgb, other.rgb)))
         )
-#         return RGBColor(
-#             min(255, self_rgb.red + other_rgb.red),
-#             min(255, self_rgb.green + other_rgb.green),
-#             min(255, self_rgb.blue + other_rgb.blue),
-#         )
 
     __add__ = add
 
     def divide(self, other):
-        self_rgb = self.rgb
-        other_rgb = other.rgb
-        if 0 in other_rgb:
-            raise ZeroDivisionError
 
         return Color(
             rgb=tuple(map(lambda x, y: x / float(y)),
-                          zip(self.rgb, other.rgb)))
+                          zip(self.rgb, other.rgb))
         )
-
-#         return RGBColor(
-#             self_rgb.red / float(other_rgb.red),
-#             self_rgb.green / float(other_rgb.green),
-#             self_rgb.blue / float(other_rgb.blue),
-#         )
 
     __div__ = divide
 
@@ -168,41 +146,49 @@ class Color(object):
     __sub__ = subtract
 
     def screen(self, other):
-        self_rgb = self.rgb
-        other_rgb = other.rgb
+#         self_rgb = self.rgb
+#         other_rgb = other.rgb
 
         return Color(
-            rgb=tuple(map(lambda x, y: max(255, (x - y)),
+            rgb=tuple(map(lambda x, y: 255 - (((255 - x) * (255 - y)) / 255.0),
                           zip(self.rgb, other.rgb)))
         )
-       
-        return RGBColor(
-            255 - (((255 - self_rgb.red) * (255 - other_rgb.red)) / 255.0),
-            255 - (((255 - self_rgb.green) * (255 - other_rgb.green)) / 255.0),
-            255 - (((255 - self_rgb.blue) * (255 - other_rgb.blue)) / 255.0),
-        )
+
+#         return RGBColor(
+#             255 - (((255 - self_rgb.red) * (255 - other_rgb.red)) / 255.0),
+#             255 - (((255 - self_rgb.green) * (255 - other_rgb.green)) / 255.0),
+#             255 - (((255 - self_rgb.blue) * (255 - other_rgb.blue)) / 255.0),
+#         )
 
     def difference(self, other):
-        self_rgb = self.rgb
-        other_rgb = other.rgb
-        return RGBColor(
-            abs(self_rgb.red - other_rgb.red),
-            abs(self_rgb.green - other_rgb.green),
-            abs(self_rgb.blue - other_rgb.blue),
+#         self_rgb = self.rgb
+#         other_rgb = other.rgb
+        return Color(
+            rgb=tuple(map(lambda x, y: abs(x - y), zip(self.rgb, other.rgb)))
         )
+#         return RGBColor(
+#             abs(self_rgb.red - other_rgb.red),
+#             abs(self_rgb.green - other_rgb.green),
+#             abs(self_rgb.blue - other_rgb.blue),
+#         )
 
     def overlay(self, other):
         return self.screen(self.multiply(other))
 
     def invert(self):
-        return self.difference(RGBColor(255, 255, 255))
+        return self.difference(Color(rgb=(255, 255, 255)))
 
+    # TODO: make color __hash__able and use it for comparisons
     def __eq__(self, other):
-        self_rgb = self.rgb
-        other_rgb = other.rgb
-        return self_rgb.red == other_rgb.red \
-           and self_rgb.green == other_rgb.green \
-           and self_rgb.blue == other_rgb.blue
+#         self_rgb = self.rgb
+#         other_rgb = other.rgb
+        if all(map(lambda x, y: x == y, zip(self.rgb, other.rgb))):
+            return True
+        else:
+            return False
+#         return self_rgb.red == other_rgb.red \
+#            and self_rgb.green == other_rgb.green \
+#            and self_rgb.blue == other_rgb.blue
 
     def __contains__(self, item):
         return item in self.color
@@ -238,86 +224,6 @@ class Color(object):
 #         return base % (self.__class__.__name__, ', '.join(properties))
 
 
-# class HSVColor(Color):
-#     """ Hue Saturation Value """
-# 
-#     def __init__(self, h=0, s=0, v=0):
-#         if s > 1:
-#             raise ValueError('Saturation has to be less than 1')
-#         if v > 1:
-#             raise ValueError('Value has to be less than 1')
-# 
-#         # Hue can safely circle around 1
-#         if h >= 1:
-#             h -= int(h)
-# 
-#         self._color = h, s, v
-# 
-#     @property
-#     def rgb(self):
-#         return RGBColor(*map(lambda c: c * 255, colorsys.hsv_to_rgb(*self._color)))
-# 
-#     @property
-#     def hsv(self):
-#         return self
-# 
-#     class Meta:
-#         properties = ('hue', 'saturation', 'value')
-
-
-# class RGBColor(Color):
-#     """ Red Green Blue """
-# 
-#     def __init__(self, r=0, g=0, b=0):
-#         self._color = r, g, b
-#         for c in self._color:
-#             if c < 0 or c > 255:
-#                 raise ValueError('Color values must be between 0 and 255')
-# 
-#     @property
-#     def rgb(self):
-#         return self
-# 
-#     @property
-#     def hsv(self):
-#         return HSVColor(*colorsys.rgb_to_hsv(*map(lambda c: c / 255.0, self._color)))
-# 
-#     class Meta:
-#         properties = ('red', 'green', 'blue')
-
-
-# class HexColor(RGBColor):
-#     """ Typical 6 digit hexadecimal colors.
-# 
-#     Warning: accuracy is lost when converting a color to hex
-#     """
-# 
-#     def __init__(self, hex='000000'):
-#         if len(hex) != 6:
-#             raise ValueError('Hex color must be 6 digits')
-# 
-#         hex = hex.lower()
-#         if not set(hex).issubset(HEX_RANGE):
-#             raise ValueError('Not a valid hex number')
-# 
-#         self._color = hex[:2], hex[2:4], hex[4:6]
-# 
-#     @property
-#     def rgb(self):
-#         return RGBColor(*[int(c, 16) for c in self._color])
-# 
-#     @property
-#     def hsv(self):
-#         return self.rgb.hsv
-# 
-#     @property
-#     def hex(self):
-#         return self
-# 
-#     def __str__(self):
-#         return '%s%s%s' % self._color
-
-
 class ColorWheel(object):
     """ Iterate random colors disributed relatively evenly
     around the color wheel.
@@ -349,17 +255,6 @@ class ColorWheel(object):
         return HSVColor(self._phase, 1, 0.8)
 
 
-# def random():  # This name might be a bad idea?
-#     """ Generate a random color.
-# 
-#     >>> from colors import random
-#     >>> random()
-#     <HSVColor hue: 0.310089903395, saturation: 0.765033516918, value: 0.264921257867>
-#     >>> print '#%s' % random().hex
-#     #ae47a7
-# 
-#     """
-#     return HSVColor(random_.random(), random_.random(), random_.random())
 
 # Simple aliases
 # rgb = RGBColor  # rgb(100, 100, 100), or rgb(r=100, g=100, b=100)
